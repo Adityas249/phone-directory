15887474f7a20de11c83a20c3221ae5d40ef454c
Adityas249 <you@example.com>
2019-12-09 16:39:06 +0530

    Codebase Cleanup

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import React from 'react';
 
 
 function App() {
+  render() {
   return (
  <div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      
     </div>
   );
 }
